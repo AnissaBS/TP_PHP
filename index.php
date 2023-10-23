78ead91dc3cbe73903ae3154f63b66ac3efcd664
AnissaBS <139979527+AnissaBS@users.noreply.github.com>
2023-10-23 10:14:20 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -3,16 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styleIndex.css">
+    <link rel="stylesheet" href="style.css">
     <title>Document</title>
 </head>
 <body>
-    <h1>TP PHP</h1>
-    <div class="boutons">
-        <button><a href="exo1.php">Exercice 1</a></button>
-        <button><a href="exo2.php">Exercice 2</a></button>
-        <button><a href="exo3.php">Exercice 3</a></button>
-    </div>
+    <h2>Exercice 1</h2>
+    <?php
+        $dep = 18000;
+        while ($dep <= 18999) {
+            echo $dep." ";
+            $dep++;
+        }
+    ?>
 
+<h2>Exercice 2</h2>
+    <?php
+        $n = 5;
+        for ($i = 1; $i <= 10; $i++) {
+            echo $n.' x '.$i.' = '.($n * $i).'<br>';
+        }
+
+        /*
+        // AUTRE SOLUTION
+        $chiffre = 5;
+        echo "Table de multiplication du chiffre $chiffre : <br><br>";
+
+        for ($i=0; $i<=10; $i++) {
+            $result = $chiffre * $i;
+            echo "$chiffre x $i = $result <br>";
+        }
+        */
+    ?>
+
+<h2>Exercice 3</h2>
+    <?php
+        for ($i=1; $i<=5; $i++) {
+            for ($k=1; $k<=$i; $k++) {
+                echo "$i <br>";
+            }
+        }
+    ?>
+
+<h2>Exercice 4</h2>
+    <?php
+        $var = 0;
+        while($var<=20){
+            echo $var."<br>";
+            $var=$var+2;
+        }
+    ?>
 </body>
 </html>
